cd93b2d3d37e18a95e044f16f45b3b8e1231d6b3
Erythrocyte3803 <2544390577@qq.com>
2023-11-19 22:28:21 +0900

    update

--- AI Hobbyist Version/3_Get_DiffSinger_Label.py
+++ AI Hobbyist Version/3_Get_DiffSinger_Label.py
@@ -1,25 +1,24 @@
-import json
-import re
-import os
-import argparse
-import tqdm as tq
+import json, re, os, argparse
+from tqdm import tqdm
 from pathlib import Path
+from glob import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--source', type=str, help='未整理数据集目录', required=True)
-parser.add_argument('--index', type=str, help='索引路径', required=True)
+parser.add_argument('--ver', type=str, help='版本', required=True)
 parser.add_argument('--dest', type=str, help='目标路径', required=True)
-parser.add_argument('--lang', type=str, help='语言（可选CHS/EN/JP/KR，默认为CHS）', default="CHS")
+parser.add_argument('--lang', type=str, help='语言（可选CHS/EN/JP/KR）', required=True)
 args = parser.parse_args()
 
 source = str(args.source)
 dest = str(args.dest)
-index = str(args.index)
-lang = str(args.lang)
+language = str(args.lang).upper()
+ver = str(args.ver)
 monster = 'monster'
 battle = 'battle|life'
 conv = 'fetter'
 player = '旅行者|旅人|Traveler|여행자'
+
 
 renameDict = {
     '万叶': '枫原万叶',
@@ -51,33 +50,61 @@ def has_vaild_content(text):
         return True
     else:
         return False
+    
+def get_support_ver():
+    indexs = glob('./Indexs/*')
+    support_vers = []
+    for vers in indexs:
+        version = os.path.basename(vers)
+        support_vers.append(version)
+    versions = '|'.join(support_vers)
+    return versions
+
+def get_support_lang(version):
+    if is_in(version, get_support_ver()):
+        support_langs = []
+        indexs = glob(f'./Indexs/{version}/*')
+        for langs in indexs:
+            lang_code = os.path.basename(langs).replace("_output.json","").replace(".json","")
+            support_langs.append(lang_code)
+        return support_langs
+    else:
+        print("不支持的版本")
+        exit()
+    
+def get_path_by_lang(lang):
+    langcodes = get_support_lang(ver)
+    path = ['中文 - Chinese', '英语 - English',  '日语 - Japanese', '韩语 - Korean']
+    try:
+        i = langcodes.index(lang)
+        dest_path = path[i]
+        lang_code = lang
+    except:
+        print("不支持的语言")
+        exit()
+    return lang_code, dest_path
+
+langcode, dest_lang = get_path_by_lang(language)
+
 def ren_player(player,lang):
+    langcodes = get_support_lang(ver)
+    player_boy_names = ['空', 'Aether', '空', '아이테르']
+    player_girl_names = ['荧', 'Lumine', '蛍', '루미네']
     p_name = player
     if p_name == "PlayerBoy" or p_name == "PlayerGirl":
         if p_name == "PlayerBoy":
-            if lang == "CHS":
-                p_name = "空"
-            if lang == "EN":
-                p_name = "Aether"
-            if lang == "JP":
-                p_name = "空"
-            if lang == "KR":
-                p_name = "아이테르"
+            i = langcodes.index(lang)
+            p_name = player_boy_names[i]
         if p_name == "PlayerGirl":
-            if lang == "CHS":
-                p_name = "荧"
-            if lang == "EN":
-                p_name = "Lumine"
-            if lang == "JP":
-                p_name = "蛍"
-            if lang == "KR":
-                p_name = "루미네"
+            i = langcodes.index(lang)
+            p_name = player_girl_names[i]
     else:
         p_name = player
     return p_name
-f = open(index, encoding='utf8')
+
+f = open(f'./Indexs/{ver}/{langcode}.json', encoding='utf8')
 data = json.load(f)
-for k in tq.tqdm(data.keys()):
+for k in tqdm(data.keys()):
     try:
         text = data.get(k).get('voiceContent')
         char_name = data.get(k).get('talkName')
@@ -86,17 +113,17 @@ for k in tq.tqdm(data.keys()):
             if char_name in renameDict:
                 char_name = renameDict[char_name]
             if is_in(char_name, player) == True:
-                char_name = ren_player(avatar_name,lang)
+                char_name = ren_player(avatar_name,langcode)
         path = data.get(k).get('sourceFileName')
         path = path.replace(".wem",".wav")
         wav_source = source + '/' + path
         wav_file = os.path.basename(path)
         if char_name is not None:
-            vo_dest_dir = f"{dest}/角色语音 - Character/{char_name}"
-            bt_dest_dir = f"{dest}/战斗语音 - Battle/{char_name}"
-            cv_dest_dir = f"{dest}/多人对话 - Conversation/{char_name}"
-            mo_dest_dir = f"{dest}/怪物语音 - Monster/{char_name}"
-            ot_dest_dir = f"{dest}/其它语音 - Others/{char_name}"
+            vo_dest_dir = f"{dest}/{dest_lang}/角色语音 - Character/{char_name}"
+            bt_dest_dir = f"{dest}/{dest_lang}/战斗语音 - Battle/{char_name}"
+            cv_dest_dir = f"{dest}/{dest_lang}/多人对话 - Conversation/{char_name}"
+            mo_dest_dir = f"{dest}/{dest_lang}/怪物语音 - Monster/{char_name}"
+            ot_dest_dir = f"{dest}/{dest_lang}/其它语音 - Others/{char_name}"
             lab_file = wav_file.replace(".wav",".lab")
             vo_lab_path = f"{vo_dest_dir}/{lab_file}"
             bt_lab_path = f"{bt_dest_dir}/{lab_file}"
